b02a27b36ef3abce78e667bb871d601f7c91078a
Kristian Lee <kristian.lee@gmail.com>
2017-06-23 16:19:23 +0100

    finished 1x public + private subnets + NAT

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # wordpresssite
 Learning how to build a wordpress site using Terraform + Ansible
+
+## Credits
+Network configuration used content from [here](https://nickcharlton.net/posts/terraform-aws-vpc.html)
--- network.tf
+++ network.tf
@@ -23,3 +23,135 @@ resource "aws_subnet" "eu-west-2a-private" {
     Name = "Primary Private Subnet"
   }
 }
+
+resource "aws_route_table" "eu-west-2a-private" {
+  vpc_id = "${aws_vpc.tools.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    instance_id = "${aws_instance.nat.id}"
+  }
+
+  tags {
+    Name = "Private Subnet"
+  }
+}
+
+resource "aws_route_table_association" "eu-west-2a-private" {
+  subnet_id = "${aws_subnet.eu-west-2a-private.id}"
+  route_table_id = "${aws_route_table.eu-west-2a-private.id}"
+}
+
+
+#create public subnet
+
+resource "aws_subnet" "eu-west-2a-public" {
+  vpc_id = "${aws_vpc.tools.id}"
+
+  cidr_block = "${var.public1_subnet_cidr}"
+  availability_zone = "eu-west-2a"
+
+  tags {
+    Name = "Public Subnet"
+  }
+}
+
+
+resource "aws_route_table" "eu-west-2a-public" {
+  vpc_id = "${aws_vpc.tools.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.tools.id}"
+  }
+
+  tags {
+    Name = "Public Subnet"
+  }
+}
+
+#nat instance
+
+resource "aws_instance" "nat" {
+  ami = "ami-30913f47"
+  availability_zone = "eu-west-2a"
+  instance_type = "m1.small"
+  key_name = "${var.aws_key_name}"
+  vpc_security_group_ids = ["${aws_security_group.nat.id}"]
+  subnet_id = "${aws_subnet.eu-west-2a-public.id}"
+  associate_public_ip_address = true
+  source_dest_check = false
+
+  tags {
+    Name = "VPC NAT"
+  }
+}
+
+resource "aws_security_group" "nat" {
+  name = "vpc_nat"
+  description = "Allows traffic from private subnet to internet"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["${var.private1_subnet_cidr}"]
+  }
+  
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["${var.private1_subnet_cidr}"]
+  }
+
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["${var.vpc_cidr}"]
+  }
+
+  egress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id = "${aws_vpc.tools.id}"
+
+  tags {
+    Name = "NATSG"
+  }
+}
+
